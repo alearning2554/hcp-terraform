94e3a6f1bd8cf35f65200b64985141027ec474cb
alearning2554 <145651034+alearning2554@users.noreply.github.com>
2025-09-24 17:03:48 +0530

    Update bucket.tf

--- bucket.tf
+++ bucket.tf
@@ -1,6 +1,7 @@
 provider "google" {
   project = "windy-skyline-467604-b5"   # Replace with your Google Cloud project ID
   region  = "us-central1"            # Replace with your desired region
+  
 }
 
 terraform {
