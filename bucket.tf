e189d64d1a4cbcf6ad24b9dac9d242a00e1a73ef
alearning2554 <145651034+alearning2554@users.noreply.github.com>
2025-09-24 11:32:27 +0530

    Create bucket.tf

---
+++ bucket.tf
@@ -0,0 +1,11 @@
+provider "google" {
+  project = "windy-skyline-467604-b5"   # Replace with your Google Cloud project ID
+  region  = "us-central1"            # Replace with your desired region
+}
+
+resource "google_storage_bucket" "example_bucket" {
+  name          = "hcp-terraform-123"  # Replace with a unique bucket name
+  location      = "US"                     # You can change the location to suit your needs
+  storage_class = "STANDARD"               # You can use different storage classes like "NEARLINE", "COLDLINE", etc.
+  force_destroy = true                     # This allows Terraform to delete non-empty buckets (be careful!)
+}
